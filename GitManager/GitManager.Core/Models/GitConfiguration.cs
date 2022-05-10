828fdab30f52d03919478c0ee48c5cf45edac806
= <=>
2022-05-10 14:21:18 +0200

    Create GitConfiguration class and GitConfigurationScope enum

---
+++ GitManager/GitManager.Core/Models/GitConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace GitManager.Core.Models
+{
+    public class GitConfiguration
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public GitConfigurationScope Scope { get; set; }
+    }
+
+    public enum GitConfigurationScope
+    {
+        Local,
+        Global
+    }
+}

e2274a527a4ca7d10792bf75c1d79eecbf17b2fa
= <=>
2022-05-10 14:24:07 +0200

    Create GitHelper which implement IGitHelper

---
+++ GitManager/GitManager.Core/Helpers/GitHelper.cs
@@ -0,0 +1,20 @@
+﻿using GitManager.Core.Helpers.Interfaces;
+using GitManager.Core.Models;
+using GitManager.Core.Services;
+
+namespace GitManager.Core.Helpers
+{
+    public class GitHelper : IGitHelper
+    {
+        private readonly TerminalService _terminalService;
+        public GitHelper()
+        {
+            _terminalService = new TerminalService();
+        }
+
+        public GitConfiguration GetConfiguration(GitConfigurationScope scope = GitConfigurationScope.Global)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

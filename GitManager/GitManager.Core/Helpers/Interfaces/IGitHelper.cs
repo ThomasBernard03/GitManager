f2de7db39c150814c5573912598156959f96bcbe
= <=>
2022-05-10 14:23:02 +0200

    Create IGitHelper

---
+++ GitManager/GitManager.Core/Helpers/Interfaces/IGitHelper.cs
@@ -0,0 +1,13 @@
+﻿using GitManager.Core.Models;
+
+namespace GitManager.Core.Helpers.Interfaces
+{
+    public interface IGitHelper
+    {
+        /// <summary>
+        /// Get the git configuation
+        /// </summary>
+        /// <returns>The git configuration</returns>
+        GitConfiguration GetConfiguration(GitConfigurationScope scope = GitConfigurationScope.Global);
+    }
+}

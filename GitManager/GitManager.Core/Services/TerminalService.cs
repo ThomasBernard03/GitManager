93c23ea3ff2fe02de557b90056f8784178fcfb33
= <=>
2022-05-10 14:23:35 +0200

    Add TerminalService

---
+++ GitManager/GitManager.Core/Services/TerminalService.cs
@@ -0,0 +1,26 @@
+﻿using ToolBox.Bridge;
+using ToolBox.Platform;
+
+namespace GitManager.Core.Services
+{
+    public class TerminalService
+    {
+        public static IBridgeSystem _bridgeSystem { get; set; }
+        public static ShellConfigurator _shell { get; set; }
+
+        public TerminalService()
+        {
+            switch (OS.GetCurrent())
+            {
+                case "win":
+                    _bridgeSystem = BridgeSystem.Bat;
+                    break;
+                case "mac":
+                case "gnu":
+                    _bridgeSystem = BridgeSystem.Bash;
+                    break;
+            }
+            _shell = new ShellConfigurator(_bridgeSystem);
+        }
+    }
+}

8f0a54306ef3f91a47a6b0220c113771a6348db4
Thomas Bernard <thomas.bernard.310@gmail.com>
2022-05-30 09:57:09 +0200

    Create maui project

---
+++ GitManager/GitManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GitManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GitManager/GitManager/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GitManager
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
